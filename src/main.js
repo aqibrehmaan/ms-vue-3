d8c2aecf08e330a60473f8a6aa6752c97855d21e
Aqib <aqibr777@gmail.com>
2022-08-20 20:14:36 +0500

    15-rendering-multiple-routes-with-named-router-views

--- src/main.js
+++ src/main.js
@@ -5,17 +5,25 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
-
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UserFooter from './components/users/UserFooter.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/teams'},
-        { path: '/teams', name: 'teams', component: TeamsList, children: [
+        { path: '/teams', name: 'teams', components: {
+         default: TeamsList,
+         footer: TeamsFooter
+        }, children: [
             { path: ':teamId', name: 'team-members', component: TeamMembers, props: true },
         ] }, // our-domain.com/teams => TeamsList
-        { path: '/users', component: UsersList },
+        { path: '/users', components: {
+            default: UsersList,
+            footer: UserFooter
+         }
+        },
         { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active'
